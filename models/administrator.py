5b13c56b7183d31744dc2c61a067eb65499c0622
kollie <zikollie@gmail.com>
2025-03-27 23:16:52 +0100

    fix teacher id

--- models/administrator.py
+++ models/administrator.py
@@ -2,7 +2,6 @@ from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
 class Administrator(models.Model):
-    _name = 'lms.administrator'
     _inherit = 'res.users'
     _description = 'Administrator'
 
